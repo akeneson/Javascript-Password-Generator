fca7448a85381e64c3e46ed8013b394da2168057
Amy Keneson <amy.l.keneson@gmail.com>
2020-07-16 18:21:06 -0500

    Varible upperCaseCharactersArray created

--- script.js
+++ script.js
@@ -13,11 +13,16 @@ var passwordLength = prompt("How many characters long would you lik your passwor
 // This will be the boolean variable to confirm if the user would like to include lowercase characters
 
 var lowerCase = confirm("Would you like your password to contain lowercase letters?");
+
+// This variable holds an array of lower case letters
 var lowerCaseCharactersArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
 
 // This will be the boolean variable to confirm if the user would like to include uppercase characters
 
 var upperCase = confirm("Would you like your password to contain uppercase letters?");
+
+// This variable holds an array of upper case letters
+var upperCaseCharactersArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]; 
 
 // This will be the boolean variable to confirm if the user would like to include special characters
 
