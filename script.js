e71057da5b6bda0670bc452df68f67453cc411d5
Amy Keneson <amy.l.keneson@gmail.com>
2020-07-16 18:03:32 -0500

    Variable password length created

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// This will be the variable for the prompt that will hold the desire length of the password (8-128 characters)
+
+var passwordLength = prompt("How many characters long would you lik your password to be? (8-128 characters)");
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
