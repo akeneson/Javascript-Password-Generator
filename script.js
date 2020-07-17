6a897ce0d18692063d7915c4750f0a064b012d88
Amy Keneson <amy.l.keneson@gmail.com>
2020-07-17 18:03:50 -0500

    Organizing arrays | prompts | and random functions

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// This will be the variable for the prompt that will hold the desire length of the password (8-128 characters)
+// ------------------>GLOBAL VARIABLES<--------------------
+// This variable holds an array of lower case letters
+var lowerCaseCharactersArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
+// This variable holds an array of upper case letters
+var upperCaseCharactersArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]; 
+// This variable holds an array of special characters
+var specialCharactersArray = ["`","!","@","#","$","%","^","&","*","(",")","-","+","=","<",">","/","?",":",";","{","}","[","]"];
+// This variable holds am array of numbers
+var numberCharacterArray = ["0", "1", "2", "3", "4," , "5", "6", "7", "8", "9"];
 
+
+// ------------------>Prompts <--------------------
+// This will be the variable for the prompt that will hold the desire length of the password (8-128 characters)
 var passwordLength = prompt("How many characters long would you lik your password to be? (8-128 characters)");
 
 // ---------To ensure that password is between 8 and 128 (unsure if it will work)  ALSO need to see how to stop the code if an invalid number is entered again
@@ -10,55 +21,59 @@ var passwordLength = prompt("How many characters long would you lik your passwor
 //   var passwordlength = prompt("Please enter a number between 8 and 128 for which you would like the length of your password to be")
 // }
 
+
 // This will be the boolean variable to confirm if the user would like to include lowercase characters
-
 var lowerCase = confirm("Would you like your password to contain lowercase letters?");
-
-// This variable holds an array of lower case letters
-var lowerCaseCharactersArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
-
-// This variable grabs a random uppercase letter from the array
-var myLower= lowerCaseCharactersArray[Math.floor(Math.random() * lowerCaseCharactersArray.length)];
 
 // This will be the boolean variable to confirm if the user would like to include uppercase characters
 var upperCase = confirm("Would you like your password to contain uppercase letters?");
 
-// This variable holds an array of upper case letters
-var upperCaseCharactersArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]; 
-
-// This variable grabs a random uppercase letter from the array
-var myUpper = upperCaseCharactersArray[Math.floor(Math.random() * upperCaseCharactersArray.length)];
-
 // This will be the boolean variable to confirm if the user would like to include special characters
 var specialCharacter = confirm("Would you like your password to contain special characters");
 
-// This variable holds an array of special characters
-var specialCharactersArray = ["`","!","@","#","$","%","^","&","*","(",")","-","+","=","<",">","/","?",":",";","{","}","[","]"];
+var numericCharacter = confirm ("Would you like your password to contain a number");
+
+
+
+// ------------------>Random Character <--------------------
+// This variable grabs a random uppercase letter from the array
+var myLower= lowerCaseCharactersArray[Math.floor(Math.random() * lowerCaseCharactersArray.length)];
+console.log(myLower);
+
+// This variable grabs a random uppercase letter from the array
+var myUpper = upperCaseCharactersArray[Math.floor(Math.random() * upperCaseCharactersArray.length)];
+console.log(myUpper);
 
 // This variable grabs a random special character from the list of array
 var mySpecial = specialCharactersArray[Math.floor(Math.random() * specialCharactersArray.length)];
+console.log(mySpecial);
 
+// This variable grabs a random numeric character
+var myNumeric = numberCharacterArray[Math.floor(Math.random() * numberCharacterArray.length)];
+console.log(myNumeric);
 
 // Output Loop to validate that atleast one character type is selected
-for (lowerCase==true || upperCase==true || specialCharacter==true){
-  if (lowerCase==false && upperCase==false && specialCharacter==false){
-    var undefined = confirm ("Unable to create characters without chosing one character")
-    break;
-  }
-  // Output loop to validate users choices
-  // If all 3 are true
-  if (lowerCase== true && upperCase==true && specialCharacter==true); {
-    var confirmLowerUpperSpecial = confirm ("You would like to use lowercase letters, uppercase letters, and special characters");
-
-    // This variables holds an array of the chosen random characters from all three character arrays
-    var randomLowerUpperSpecial = [myLower, myUpper, mySpecial];
-  }
+// if (lowerCase==false && upperCase==false && specialCharacter==false){
+//     var undefined = confirm ("Unable to create characters without chosing one character");
+//     break;
+// }
+//   // Output loop to validate users choices
+//   // If all 3 are true
+if (lowerCase== true && upperCase==true && specialCharacter==true); {
+      var confirmLowerUpperSpecial = confirm ("You would like to use lowercase letters, uppercase letters, and special characters");
+      // STILL LOGGING TRUE, WHEN FALSE.
+      console.log(confirmLowerUpperSpecial);
 }
 
 
-    // This variable chooses from the character from the above array
-    var random3 = randomLowerUpperSpecial[Math.floor(Math.random() * randomLowerUpperSpecial.length)];
-    break;
+
+//     // This variables holds an array of the chosen random characters from all three character arrays
+//     var randomLowerUpperSpecial = [myLower, myUpper, mySpecial];
+
+//     // This variable chooses from the character from the above array
+//     var random3 = randomLowerUpperSpecial[Math.floor(Math.random() * randomLowerUpperSpecial.length)];
+//     break;
+// }
  
 // --------------------------
 // If the user wants lower and upper case only
@@ -96,20 +111,20 @@ for (lowerCase==true || upperCase==true || specialCharacter==true){
 // Write password to the #password input
 
 
-function generatePassword(){
-  for (i=0; i<passwordLength-1; i++){ 
-    var password = password + random3; 
-  }
-}
+// function generatePassword(){
+//   for (i=0; i<passwordLength-1; i++){ 
+//     var password = password + random3; 
+//   }
+// }
 
-function writePassword() {
-  var password = generatePassword();
+// function writePassword() {
+//   var password = generatePassword();
   
-  var passwordText = document.querySelector("#password");
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
 // Add event listener to generate button
 
